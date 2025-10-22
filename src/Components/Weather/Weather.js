47970e6bbc856ec22e986b29eb5bdd821193c3b7
tarek <tarekelshenawy1997@gmail.com>
2025-10-22 18:08:31 +0300

    change style

--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -1,3 +1,4 @@
+// Refactored React Weather Component
 import React, { useEffect, useRef, useState } from 'react';
 import "./Weather.css";
 import searchicon from "../images/search.png";
@@ -10,149 +11,117 @@ import snow from "../images/snow.png";
 import clouds from "../images/clouds.png";
 import rain from "../images/rain.png";
 import { Audio } from 'react-loader-spinner';
+import logo from '../images/logo.svg';
 
 export default function Weather() {
+  const [weather, setWeather] = useState({
+    temperature: 2,
+    country: "london",
+    humidity: "25",
+    windSpeed: "48",
+  });
+  const [image, setImage] = useState(sunny);
+  const [loading, setLoading] = useState(true);
+  const inputRef = useRef();
+  const apikey = process.env.REACT_APP_WEATHER_API_KEY;
 
-    const [state,setState]=useState({
-        celicius:2,
-        country:"london",
-        humidity:"25%",
-        windspeed:"48",
-    })
-    const [image,setImage]=useState(sunny);
-    const[loading,setLoading]=useState(true);
-    let inputref = useRef();
-     let apikey = process.env.REACT_APP_WEATHER_API_KEY;
-    async function showcity(city){
-        try {
-         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apikey}`);
-        var data = await response.json();
-        if(response.status === 404){
-           document.querySelector(".display").style.display ="none";
-           document.getElementById("valid").innerHTML="valid value";
-        }
-        else{
-            document.querySelector(".display").style.display ="block";
-            document.getElementById("valid").innerHTML="";
-        
-            if(data.weather[0].main === "Clear"){
-                setImage(sunny)
-           }else if(data.weather[0].main === "Drizzle"){
-             setImage(drizzle)
-        
-           }
-           else if(data.weather[0].main === "Mist"){
-            setImage(mist)
-           }
-           else if(data.weather[0].main === "Snow"){
-            setImage(snow)
+  const imageMap = {
+    Clear: sunny,
+    Drizzle: drizzle,
+    Mist: mist,
+    Snow: snow,
+    Clouds: clouds,
+    Rain: rain,
+  };
 
-            }
-            else if(data.weather[0].main === "Clouds"){
-                setImage(clouds)
+  async function fetchCityWeather(city) {
+    try {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apikey}`);
+      const data = await response.json();
 
-            }
-             else if(data.weather[0].main === "Rain"){
-                    setImage(rain)
-             }
-            setState({
-                celicius :data.main.temp,
-                country:data.name,
-                humidity:data.main.humidity,
-                windspeed:data.wind.speed,   
-            })
-        
-           
-           
-        }
-       
-        }
-        catch(error){
-        console.log(error.message)
-        }
-        finally{
-            setLoading(false)
-        }
-               
-        
+      if (response.status === 404) {
+        document.querySelector(".display").style.display = "none";
+        document.getElementById("valid").innerHTML = "Invalid city name";
+      } else {
+        document.querySelector(".display").style.display = "block";
+        document.getElementById("valid").innerHTML = "";
 
-        }
+        const weatherMain = data.weather[0].main;
+        setImage(imageMap[weatherMain] || sunny);
 
-         useEffect(()=>{
-        showcity("egypt")
-
-    },[])
-       
-    function showweather(){
-        let city = inputref.current.value;
-        showcity(city);
-        inputref.current.value="";
-
+        setWeather({
+          temperature: data.main.temp,
+          country: data.name,
+          humidity: data.main.humidity,
+          windSpeed: data.wind.speed,
+        });
+      }
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setLoading(false);
     }
+  }
+
+  useEffect(() => {
+    fetchCityWeather("egypt");
+  }, []);
+
+  function handleSearch() {
+    const city = inputRef.current.value;
+    if (city) fetchCityWeather(city);
+    inputRef.current.value = "";
+  }
+
   return (
     <div className='container'>
-        { loading ?
-                    <div style={{width:"100%",height:"100vh",
-              display:"flex",justifyContent:"center",alignItems:"center"}}>
-             <Audio
-                  height="80"
-                  width="80"
-                  radius="9"
-                  color="green"
-                  ariaLabel="loading"
-                  wrapperStyle
-                  wrapperClass
-                
-                />
-         </div>
-
-
-        :
-             <div className='weather_content'>
-
-                        <div className='search'>
-                                <input type='text' placeholder='Search' ref={inputref} ></input>
-                                <img src={searchicon} alt='' width="40px" onClick={showweather}></img>
-                            
-                            </div>
-                            <p id="valid"></p>
-
-                        <div className='display'>
-
-                            <img src={image} alt='' className='weathericon' width="90px"></img>
-                            <div className='country'>
-                            <p>{Math.round(state.celicius)} °C</p>
-                            <h3>{state.country}</h3>
-
-                            </div>
-                        
-                            <div className='weatherinfo'>
-                                <div className='humidity'>
-                                    <img src={humidity} alt='' ></img>
-                                    <div >
-                                        <p>{state.humidity}%</p>
-                                        <p>humidity</p>
-                                    </div>
-
-                                </div>
-                                <div className='windspeed'>
-                                    <img src={windspeed} alt='' ></img>
-                                    <div >
-                                        <p>{Math.round(state.windspeed)}%</p>
-                                        <p>wind speed</p>
-                                    </div>
-
-                                </div>
-
-                            </div>
-                        
-                        </div>
-
-             </div>
-
-      
-                    }
-
+      {loading ? (
+        <div style={{ width: "100%", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <Audio height="80" width="80" ariaLabel="loading" />
+        </div>
+      ) : (
+        <div>
+          <header>
+            <div className='title'>
+              <img src={logo} alt='weather logo' className='weather_logo' />
+            </div>
+          </header>
+          <div className='weather_content'>
+            <h1>How's the sky looking today?</h1>
+            <div className='search'>
+              <input type='text' placeholder='Search city' ref={inputRef} />
+              <img src={searchicon} alt='search icon' width='40' onClick={handleSearch} />
+            </div>
+            <p id='valid'></p>
+            <div className='country-details'>
+              <h3>{weather.country}</h3>
+              <div className='country-degree'>
+                <img src={image} alt='weather icon' className='weathericon' width='90' />
+                <p>{Math.round(weather.temperature)} °C</p>
+              </div>
+            </div>
+            <div className='display'>
+              <div className='weatherinfo'>
+                <div className='humidity'>
+                  <img src={humidity} alt='humidity icon' />
+                  <div>
+                    <p>{weather.humidity}%</p>
+                    <p>Humidity</p>
+                  </div>
+                </div>
+                <div className='windspeed'>
+                  <img src={windspeed} alt='wind speed icon' />
+                  <div>
+                    <p>{Math.round(weather.windSpeed)} km/h</p>
+                    <p>Wind Speed</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
+
